9a792ca8469319baf160c4e0479ae29a806558a7
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-01-25 00:31:53 +0900

    Delete files related with directory organisation

--- python/main.py
+++ python/main.py
@@ -10,24 +10,6 @@ from sidebar import Sidebar
 
 print('==================== Categorizing the Entire aya-issues Wiki Pages... ====================')
 
-print('========== Tidying Directories... ==========')
-mkdir_results, mv_wikis_to_dir_results, delete_empty_dir_results = Directory().run()
-print('===== Making Directories by Owner Unit... =====')
-for mkdir_result in mkdir_results:
-    print(mkdir_result)
-print('===== Done Making Directories by Owner Unit 🎉 =====\n')
-
-print("===== Moving Files to Owner's Directories... =====")
-for mv_wikis_to_dir_result in mv_wikis_to_dir_results:
-    print(mv_wikis_to_dir_result)
-print("===== Done Moving Files to Owner's Directories 🎉 =====\n")
-
-print('===== Deleting Empty Directories... =====')
-for delete_empty_dir_result in delete_empty_dir_results:
-    print(delete_empty_dir_result)
-print('===== Done Deleting Empty Directories 🎉 =====')
-print('========== Done Tidying Directories 🎉 ==========\n')
-
 print('========== Tidying Home... ==========')
 home_url = Home().run()
 print('Check out An Up-to-date Wiki List on Home at {home_url} !!'.format(home_url = home_url))
