f2a203dd233364eb5efe67a634c03dd270b7a2c3
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-07-25 18:19:56 +0900

    Switch order of namespace on unit tests

--- python/test/test_home.py
+++ python/test/test_home.py
@@ -20,6 +20,72 @@ class TestHome(TestApplication):
             Home(base_path = self.base_path, genre = '-o', template_lang = 'spa')
         self.assertEqual(str(e.exception), 'Unknown template_lang: `spa`')
 
+class English(TestHome):
+    class OwnedHomeTest(TestHome):
+        def test_run(self):
+            self.assertEqual(self.home, self.__home_passage__())
+
+        # private
+
+        def __home_passage__(self):
+            passage  = '## How to Manage Wiki Pages\n'
+            passage += '\n'
+            passage += 'This Home page manage wikis by owner group.\n'
+            passage += '\n'
+            passage += 'Absence of ownership declaration worsens maintainability and searchability because it makes ambiguous which team the responsibility belongs to.  \n'
+            passage += 'Kindly make sure to articulate `Owner: @OWNER_TEAM` of the top of each of your wiki page to avoid it.\n'
+            passage += '\n'
+            passage += 'Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n'
+            passage += '\n'
+            passage += '## [@test-owner](https://github.com/orgs/hayat01sh1da/teams/test-owner)\n'
+            passage += '\n'
+            passage += '- [[Owner記名ありページ]]\n'
+            passage += '\n'
+            passage += '## Ownerチームが不明だが必要なページ群\n'
+            passage += '\n'
+            passage += '- [[Ownerチームが不明だが必要なページ]]\n'
+            passage += '\n'
+            passage += '## Ownerチーム・要or不要が不明なページ群\n'
+            passage += '\n'
+            passage += '- [[Ownerチーム・要or不要が不明なページ]]\n'
+            passage += '\n'
+            passage += '## Owner記名なし\n'
+            passage += '\n'
+            passage += '- [[Owner記名なしページ1]]\n'
+            passage += '- [[Owner記名なしページ2]]\n'
+
+            return passage
+
+    class PlainHomeTest(TestHome):
+        def setUp(self):
+            super().setUp(genre = '-c')
+
+        def test_run(self):
+            self.assertEqual(self.home, self.__home_passage__())
+
+        # private
+
+        def __home_passage__(self):
+            passage  = '## How to Manage Wiki Pages\n'
+            passage += '\n'
+            passage += 'This Home page manage wikis by category group.\n'
+            passage += '\n'
+            passage += 'Absence of category declaration worsens maintainability and searchability.  \n'
+            passage += 'Kindly make sure to articulate `Category: CATEGORY_NAME` of the top of each of your wiki page to avoid it.\n'
+            passage += '\n'
+            passage += 'Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n'
+            passage += '\n'
+            passage += '## test-category\n'
+            passage += '\n'
+            passage += '- [[Category記載ありページ]]\n'
+            passage += '\n'
+            passage += '## Category記載なし\n'
+            passage += '\n'
+            passage += '- [[Category記載なしページ1]]\n'
+            passage += '- [[Category記載なしページ2]]\n'
+
+            return passage
+
 class Japanese(TestHome):
     class OwnedHomeTest(TestHome):
         def setUp(self):
@@ -77,72 +143,6 @@ class Japanese(TestHome):
             passage += '治安維持のため、各ページの冒頭に `Category: カテゴリー名` を明記して頂きますようよろしくお願いします。\n'
             passage += '\n'
             passage += 'なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n'
-            passage += '\n'
-            passage += '## test-category\n'
-            passage += '\n'
-            passage += '- [[Category記載ありページ]]\n'
-            passage += '\n'
-            passage += '## Category記載なし\n'
-            passage += '\n'
-            passage += '- [[Category記載なしページ1]]\n'
-            passage += '- [[Category記載なしページ2]]\n'
-
-            return passage
-
-class English(TestHome):
-    class OwnedHomeTest(TestHome):
-        def test_run(self):
-            self.assertEqual(self.home, self.__home_passage__())
-
-        # private
-
-        def __home_passage__(self):
-            passage  = '## How to Manage Wiki Pages\n'
-            passage += '\n'
-            passage += 'This Home page manage wikis by owner group.\n'
-            passage += '\n'
-            passage += 'Absence of ownership declaration worsens maintainability and searchability because it makes ambiguous which team the responsibility belongs to.  \n'
-            passage += 'Kindly make sure to articulate `Owner: @OWNER_TEAM` of the top of each of your wiki page to avoid it.\n'
-            passage += '\n'
-            passage += 'Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n'
-            passage += '\n'
-            passage += '## [@test-owner](https://github.com/orgs/hayat01sh1da/teams/test-owner)\n'
-            passage += '\n'
-            passage += '- [[Owner記名ありページ]]\n'
-            passage += '\n'
-            passage += '## Ownerチームが不明だが必要なページ群\n'
-            passage += '\n'
-            passage += '- [[Ownerチームが不明だが必要なページ]]\n'
-            passage += '\n'
-            passage += '## Ownerチーム・要or不要が不明なページ群\n'
-            passage += '\n'
-            passage += '- [[Ownerチーム・要or不要が不明なページ]]\n'
-            passage += '\n'
-            passage += '## Owner記名なし\n'
-            passage += '\n'
-            passage += '- [[Owner記名なしページ1]]\n'
-            passage += '- [[Owner記名なしページ2]]\n'
-
-            return passage
-
-    class PlainHomeTest(TestHome):
-        def setUp(self):
-            super().setUp(genre = '-c')
-
-        def test_run(self):
-            self.assertEqual(self.home, self.__home_passage__())
-
-        # private
-
-        def __home_passage__(self):
-            passage  = '## How to Manage Wiki Pages\n'
-            passage += '\n'
-            passage += 'This Home page manage wikis by category group.\n'
-            passage += '\n'
-            passage += 'Absence of category declaration worsens maintainability and searchability.  \n'
-            passage += 'Kindly make sure to articulate `Category: CATEGORY_NAME` of the top of each of your wiki page to avoid it.\n'
-            passage += '\n'
-            passage += 'Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n'
             passage += '\n'
             passage += '## test-category\n'
             passage += '\n'
--- ruby/test/home_test.rb
+++ ruby/test/home_test.rb
@@ -21,6 +21,80 @@ class HomeTest < ApplicationTest
   attr_reader :path_to_home, :home
 end
 
+module English
+  class OwnedHomeTest < HomeTest
+    def test_self_run
+      assert_equal(home_passage.join, home)
+    end
+
+    private
+
+    def home_passage
+      [
+        "## How to Manage Wiki Pages\n",
+        "\n",
+        "This Home page manage wikis by owner group.\n",
+        "\n",
+        "Absence of ownership declaration worsens maintainability and searchability because it makes ambiguous which team the responsibility belongs to.  \n",
+        "Kindly make sure to articulate `Owner: @OWNER_TEAM` of the top of each of your wiki page to avoid it.\n",
+        "\n",
+        "Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n",
+        "\n",
+        "## [@test-owner](https://github.com/orgs/hayat01sh1da/teams/test-owner)\n",
+        "\n",
+        "- [[Owner記名ありページ]]\n",
+        "\n",
+        "## Ownerチームが不明だが必要なページ群\n",
+        "\n",
+        "- [[Ownerチームが不明だが必要なページ]]\n",
+        "\n",
+        "## Ownerチーム・要or不要が不明なページ群\n",
+        "\n",
+        "- [[Ownerチーム・要or不要が不明なページ]]\n",
+        "\n",
+        "## Owner記名なし\n",
+        "\n",
+        "- [[Owner記名なしページ1]]\n",
+        "- [[Owner記名なしページ2]]\n"
+      ]
+    end
+  end
+
+  class PlainHomeTest < HomeTest
+    def setup
+      super(genre: '-c')
+    end
+
+    def test_self_run
+      assert_equal(home_passage.join, home)
+    end
+
+    private
+
+    def home_passage
+      [
+        "## How to Manage Wiki Pages\n",
+        "\n",
+        "This Home page manage wikis by category group.\n",
+        "\n",
+        "Absence of category declaration worsens maintainability and searchability.  \n",
+        "Kindly make sure to articulate `Category: CATEGORY_NAME` of the top of each of your wiki page to avoid it.\n",
+        "\n",
+        "Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n",
+        "\n",
+        "## test-category\n",
+        "\n",
+        "- [[Category記載ありページ]]\n",
+        "\n",
+        "## Category記載なし\n",
+        "\n",
+        "- [[Category記載なしページ1]]\n",
+        "- [[Category記載なしページ2]]\n"
+      ]
+    end
+  end
+end
+
 module Japanese
   class OwnedHomeTest < HomeTest
     def setup
@@ -85,80 +159,6 @@ module Japanese
         "治安維持のため、各ページの冒頭に `Category: カテゴリー名` を明記して頂きますようよろしくお願いします。\n",
         "\n",
         "なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n",
-        "\n",
-        "## test-category\n",
-        "\n",
-        "- [[Category記載ありページ]]\n",
-        "\n",
-        "## Category記載なし\n",
-        "\n",
-        "- [[Category記載なしページ1]]\n",
-        "- [[Category記載なしページ2]]\n"
-      ]
-    end
-  end
-end
-
-module English
-  class OwnedHomeTest < HomeTest
-    def test_self_run
-      assert_equal(home_passage.join, home)
-    end
-
-    private
-
-    def home_passage
-      [
-        "## How to Manage Wiki Pages\n",
-        "\n",
-        "This Home page manage wikis by owner group.\n",
-        "\n",
-        "Absence of ownership declaration worsens maintainability and searchability because it makes ambiguous which team the responsibility belongs to.  \n",
-        "Kindly make sure to articulate `Owner: @OWNER_TEAM` of the top of each of your wiki page to avoid it.\n",
-        "\n",
-        "Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n",
-        "\n",
-        "## [@test-owner](https://github.com/orgs/hayat01sh1da/teams/test-owner)\n",
-        "\n",
-        "- [[Owner記名ありページ]]\n",
-        "\n",
-        "## Ownerチームが不明だが必要なページ群\n",
-        "\n",
-        "- [[Ownerチームが不明だが必要なページ]]\n",
-        "\n",
-        "## Ownerチーム・要or不要が不明なページ群\n",
-        "\n",
-        "- [[Ownerチーム・要or不要が不明なページ]]\n",
-        "\n",
-        "## Owner記名なし\n",
-        "\n",
-        "- [[Owner記名なしページ1]]\n",
-        "- [[Owner記名なしページ2]]\n"
-      ]
-    end
-  end
-
-  class PlainHomeTest < HomeTest
-    def setup
-      super(genre: '-c')
-    end
-
-    def test_self_run
-      assert_equal(home_passage.join, home)
-    end
-
-    private
-
-    def home_passage
-      [
-        "## How to Manage Wiki Pages\n",
-        "\n",
-        "This Home page manage wikis by category group.\n",
-        "\n",
-        "Absence of category declaration worsens maintainability and searchability.  \n",
-        "Kindly make sure to articulate `Category: CATEGORY_NAME` of the top of each of your wiki page to avoid it.\n",
-        "\n",
-        "Also, please keep in mind that you do not have to edit Home and Sidebar by yourself, which are automatically updated by a GitHub Actions cron job.\n",
         "\n",
         "## test-category\n",
         "\n",
