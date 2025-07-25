3ee3d2d0885e1c2b6dd202ed501393f631b4e6a3
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-07-25 14:51:11 +0900

    Lower cognitive load of `\n`

--- python/src/home.py
+++ python/src/home.py
@@ -26,28 +26,36 @@ class Home(Application):
     def __write_home_template__(self):
         match self.genre:
             case '-o' | '--owner':
-                self.home_passage += 'このページは Owner チームごとに Wiki をグルーピングして一覧化しています。\n\n'
-                self.home_passage += '## Wiki ページの運用ルール\n\n'
+                self.home_passage += '## Wiki ページの運用ルール\n'
+                self.home_passage += '\n'
+                self.home_passage += 'このページは Owner チームごとに Wiki をグルーピングして一覧化しています。\n'
+                self.home_passage += '\n'
                 self.home_passage += 'Ownership をどのチームが持つのかが不明だと、責任の所在が不明瞭になり、保守性の悪化に伴うノイズの増加と検索性の悪化が発生します。  \n'
-                self.home_passage += '治安維持のため、各ページの冒頭に `Owner: {オーナーチーム名}` を明記して頂きますようよろしくお願いします。  \n'
+                self.home_passage += '治安維持のため、各ページの冒頭に `Owner: {オーナーチーム名}` を明記して頂きますようよろしくお願いします。\n'
             case '-c' | '--category':
-                self.home_passage += 'このページは Category ごとに Wiki をグルーピングして一覧化しています。\n\n'
-                self.home_passage += '## Wiki ページの運用ルール\n\n'
+                self.home_passage += '## Wiki ページの運用ルール\n'
+                self.home_passage += '\n'
+                self.home_passage += 'このページは Category ごとに Wiki をグルーピングして一覧化しています。\n'
+                self.home_passage += '\n'
                 self.home_passage += 'Category が不明だと、保守性と検索性の悪化が発生します。  \n'
-                self.home_passage += '治安維持のため、各ページの冒頭に `Category: {カテゴリー名}` を明記して頂きますようよろしくお願いします。  \n'
+                self.home_passage += '治安維持のため、各ページの冒頭に `Category: {カテゴリー名}` を明記して頂きますようよろしくお願いします。\n'
 
-        self.home_passage += 'なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n\n'
+        self.home_passage += '\n'
+        self.home_passage += 'なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n'
+        self.home_passage += '\n'
 
     # @return [str]
     def __update_home_template__(self):
         for namespace, wikis in self.owned_wiki_maps.items():
-            self.home_passage += f'## [{namespace}]({self.base_owner_url + re.sub(r'@', '', namespace)})\n\n'
+            self.home_passage += f'## [{namespace}]({self.base_owner_url + re.sub(r'@', '', namespace)})\n'
+            self.home_passage += '\n'
             for wiki in wikis:
                 self.home_passage += f'- [[{re.sub(r'\.md', '', wiki)}]]\n'
             self.home_passage += '\n'
 
         for namespace, wikis in self.plain_wiki_maps.items():
-            self.home_passage += f'## {namespace}\n\n'
+            self.home_passage += f'## {namespace}\n'
+            self.home_passage += '\n'
             for wiki in wikis:
                 self.home_passage += f'- [[{re.sub(r'\.md', '', wiki)}]]\n'
             self.home_passage += '\n'
--- ruby/src/home.rb
+++ ruby/src/home.rb
@@ -24,24 +24,31 @@ class Home < Application
   def write_home_template
     case genre
     when '-o', '--owner'
-      home_passage << "このページは Owner チームごとに Wiki をグルーピングして一覧化しています。\n\n"
-      home_passage << "## Wiki ページの運用ルール\n\n"
+      home_passage << "## Wiki ページの運用ルール\n"
+      home_passage << "\n"
+      home_passage << "このページは Owner チームごとに Wiki をグルーピングして一覧化しています。\n"
+      home_passage << "\n"
       home_passage << "Ownership をどのチームが持つのかが不明だと、責任の所在が不明瞭になり、保守性の悪化に伴うノイズの増加と検索性の悪化が発生します。  \n"
-      home_passage << "治安維持のため、各ページの冒頭に `Owner: {オーナーチーム名}` を明記して頂きますようよろしくお願いします。  \n"
+      home_passage << "治安維持のため、各ページの冒頭に `Owner: {オーナーチーム名}` を明記して頂きますようよろしくお願いします。\n"
     when '-c', '--category'
-      home_passage << "このページは Category ごとに Wiki をグルーピングして一覧化しています。\n\n"
-      home_passage << "## Wiki ページの運用ルール\n\n"
+      home_passage << "## Wiki ページの運用ルール\n"
+      home_passage << "\n"
+      home_passage << "このページは Category ごとに Wiki をグルーピングして一覧化しています。\n"
+      home_passage << "\n"
       home_passage << "Category が不明だと、保守性と検索性の悪化が発生します。  \n"
-      home_passage << "治安維持のため、各ページの冒頭に `Category: {カテゴリー名}` を明記して頂きますようよろしくお願いします。  \n"
+      home_passage << "治安維持のため、各ページの冒頭に `Category: {カテゴリー名}` を明記して頂きますようよろしくお願いします。\n"
     end
 
-    home_passage << "なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n\n"
+    home_passage << "\n"
+    home_passage << "なお、Home・Sidebar は専用の定期実行ジョブで自動更新しますので編集は不要です。\n"
+    home_passage << "\n"
   end
 
   # @return nil
   def update_home_passage
     owned_wiki_maps.each { |namespace, wikis|
-      home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n\n"
+      home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n"
+      home_passage << "\n"
       wikis.each { |wiki|
         home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
       }
@@ -49,7 +56,8 @@ class Home < Application
     }
 
     plain_wiki_maps.each { |namespace, wikis|
-      home_passage << "## #{namespace}\n\n"
+      home_passage << "## #{namespace}\n"
+      home_passage << "\n"
       wikis.each { |wiki|
         home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
       }
