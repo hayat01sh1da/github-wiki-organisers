6f894b189f0fb8e63c011ee320614dc076636473
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-07-26 06:56:24 +0900

    Update execution files

--- python/exec/export_unknown_wiki_count_list_by_namespace.py
+++ python/exec/export_unknown_wiki_count_list_by_namespace.py
@@ -6,18 +6,26 @@ sys.path.append('./src')
 
 from unknown_wiki_count_list_exporter import UnknownWikiCountListExporter
 
-base_path    = os.path.join('..', '..')
-_, genre, *_ = sys.argv
+_, genre, language, *_ = sys.argv
 
-print('==================== Exporting Unknown Wiki Count List... ====================')
-print('========== Organising Home... ==========')
-count_list_by_namespace, path_to_export = UnknownWikiCountListExporter(base_path = base_path, genre = genre).run()
-print('Here is the result:\n\n')
-
-print(count_list_by_namespace)
-
-print(f"\nCheck it out result on '{path_to_export}' !!")
-print('==================== Done Exporting Unknown Wiki Count List 🎉 ====================')
+print('-------------------- Exporting Unknown Wiki Count List... --------------------')
+match genre:
+    case '-o' | '--owner' | '-c' | '--category':
+        match language:
+            case '-en' | '-ja':
+                count_list_by_namespace, path_to_export = UnknownWikiCountListExporter(genre = genre, language = language).run()
+    case _:
+        count_list_by_namespace, path_to_export = UnknownWikiCountListExporter().run()
+print()
+print('Here is the result:')
+print()
+print('---------------------------------------')
+print(count_list_by_namespace.rstrip())
+print('---------------------------------------')
+print()
+print(f"Check it out result on '{path_to_export}' !!")
+print()
+print('-------------------- Done Exporting Unknown Wiki Count List 🎉 --------------------')
 
 for pycache in glob.glob(os.path.join('.', '**', '__pycache__'), recursive = True):
     if os.path.exists(pycache):
--- python/exec/update_wiki_list_on_home_and_sidebar.py
+++ python/exec/update_wiki_list_on_home_and_sidebar.py
@@ -7,23 +7,33 @@ sys.path.append('./src')
 from home import Home
 from sidebar import Sidebar
 
-base_path    = os.path.join('..', '..')
-_, genre, *_ = sys.argv
+_, genre, language, *_ = sys.argv
 
-print('==================== Categorising the Entire github-wiki-organisers Wiki Pages... ====================')
-
-print('========== Organising Home... ==========')
-home_url = Home(base_path = base_path, genre = genre).run()
-print(f'Check out an Up-to-date Wiki List on Home at {home_url} !!')
-print('========== Done Organising Home 🎉 ==========\n')
-
-print('========== Organising Sidebar... ==========')
-Sidebar(base_path = base_path, genre = genre).run()
-print(f'Check out an Up-to-date Wiki List on Sidebar at {home_url} !!')
-print('==================== Done Categorising the Entire github-wiki-organisers Wiki Pages 🎉 ====================')
-print('========== Done Organising Sidebar 🎉 ==========\n')
-
-print('==================== Done Categorising the Entire github-wiki-organisers Wiki Pages 🎉 ====================')
+print('-------------------- Categorising the Entire github-wiki-organisers Wiki Pages... --------------------')
+print()
+print('-------------------- Organising Home... --------------------')
+match genre:
+    case '-o' | '--owner' | '-c' | '--category':
+        match language:
+            case '-en' | '-ja':
+                home_url = Home(genre = genre, language = language).run()
+    case _:
+        home_url = Home().run()
+print(f"Check out an Up-to-date Wiki List on Home at '{home_url}' !!")
+print('-------------------- Done Organising Home 🎉 --------------------')
+print()
+print('-------------------- Organising Sidebar... --------------------')
+match genre:
+    case '-o' | '--owner' | '-c' | '--category':
+        match language:
+            case '-en' | '-ja':
+                Sidebar(genre = genre, language = language).run()
+    case _:
+        Sidebar().run()
+print(f"Check out an Up-to-date Wiki List on Sidebar at '{home_url}' !!")
+print('-------------------- Done Organising Sidebar 🎉 --------------------')
+print()
+print('-------------------- Done Categorising the Entire github-wiki-organisers Wiki Pages 🎉 --------------------')
 
 for pycache in glob.glob(os.path.join('.', '**', '__pycache__'), recursive = True):
     if os.path.exists(pycache):
