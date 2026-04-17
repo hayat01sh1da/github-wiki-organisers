3aeb43dfbeefd48c06c5214ae02c17ff2dba21b4
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-18 03:16:58 +0900

    FIx [C] Metrics/AbcSize
    Assignment Branch Condition size for write_home_passage is too high. [<15, 86, 13> 88.26/88]

--- ruby/src/home.rb
+++ ruby/src/home.rb
@@ -19,7 +19,12 @@ class Home < Application
 
   # @rbs return: String
   def run
-    write_home_passage
+    if home_overflow
+      write_concise_home_passage
+    else
+      write_home_passage
+    end
+
     HOME_URL
   end
 
@@ -37,61 +42,62 @@ class Home < Application
     @home_passage ||= File.readlines(path_to_home_template).append("\n")
   end
 
+  # @rbs return: void
+  def write_home_passage
+    FileUtils.rm_rf(path_to_wikis_by_owner) if Dir.exist?(path_to_wikis_by_owner)
+
+    owned_wiki_maps.each { |namespace, wikis|
+      home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n"
+      home_passage << "\n"
+      wikis.each { |wiki|
+        home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
+      }
+      home_passage << "\n"
+    }
+
+    plain_wiki_maps.each { |namespace, wikis|
+      home_passage << "## #{namespace}\n"
+      home_passage << "\n"
+      wikis.each { |wiki|
+        home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
+      }
+      home_passage << "\n"
+    }
+
+    File.write(path_to_home, home_passage.join.chomp)
+  end
+
   # @rbs array: Array[untyped]
   # @rbs return: void
-  def write_home_passage(array = [])
-    if home_overflow
-      FileUtils.mkdir_p(path_to_wikis_by_owner) unless Dir.exist?(path_to_wikis_by_owner)
+  def write_concise_home_passage(array = [])
+    FileUtils.mkdir_p(path_to_wikis_by_owner) unless Dir.exist?(path_to_wikis_by_owner)
 
-      owned_wiki_maps.each { |namespace, wikis|
-        home_passage  = array
-        home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n"
-        home_passage << "\n"
-        wikis.each { |wiki|
-          home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
-        }
-        home_passage << "\n"
-
-        File.write(File.join(path_to_wikis_by_owner,"#{namespace}.md"), home_passage.join.chomp)
-      }
-
-      plain_wiki_maps.each { |namespace, wikis|
-        home_passage  = array.dup
-        home_passage << "## #{namespace}\n"
-        home_passage << "\n"
-        wikis.each { |wiki|
-          home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
-        }
-        home_passage << "\n"
-
-        File.write(File.join(path_to_wikis_by_owner,"#{namespace}.md"), home_passage.join.chomp)
-      }
-
-      (owned_wiki_maps.keys + plain_wiki_maps.keys).each { |namespace| home_passage << "- [[#{namespace}]]\n" }
+    owned_wiki_maps.each { |namespace, wikis|
+      home_passage  = array
+      home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n"
       home_passage << "\n"
-      File.write(path_to_home, home_passage.join.chomp)
-    else
-      FileUtils.rm_rf(path_to_wikis_by_owner) if Dir.exist?(path_to_wikis_by_owner)
-
-      owned_wiki_maps.each { |namespace, wikis|
-        home_passage << "## [#{namespace}](#{base_owner_url + namespace.gsub(/\@/, '')})\n"
-        home_passage << "\n"
-        wikis.each { |wiki|
-          home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
-        }
-        home_passage << "\n"
+      wikis.each { |wiki|
+        home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
       }
+      home_passage << "\n"
 
-      plain_wiki_maps.each { |namespace, wikis|
-        home_passage << "## #{namespace}\n"
-        home_passage << "\n"
-        wikis.each { |wiki|
-          home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
-        }
-        home_passage << "\n"
+      File.write(File.join(path_to_wikis_by_owner,"#{namespace}.md"), home_passage.join.chomp)
+    }
+
+    plain_wiki_maps.each { |namespace, wikis|
+      home_passage  = array.dup
+      home_passage << "## #{namespace}\n"
+      home_passage << "\n"
+      wikis.each { |wiki|
+        home_passage << "- [[#{wiki.gsub(/\.md/, '')}]]\n"
       }
+      home_passage << "\n"
 
-      File.write(path_to_home, home_passage.join.chomp)
-    end
+      File.write(File.join(path_to_wikis_by_owner,"#{namespace}.md"), home_passage.join.chomp)
+    }
+
+    (owned_wiki_maps.keys + plain_wiki_maps.keys).each { |namespace| home_passage << "- [[#{namespace}]]\n" }
+    home_passage << "\n"
+    File.write(path_to_home, home_passage.join.chomp)
   end
 end
